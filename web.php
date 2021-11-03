96348e6db9b78841555d9341672d48e1518e3798
wantyoui <84280050+wantyoui@users.noreply.github.com>
2021-11-03 03:46:10 +0000

    Add files via upload

---
+++ web.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>테스트 웹</title>
+</head>
+
+<body>
+        <div id="title">
+            <h1>세탁기 이용 모니터링 시스템</h1>
+        </div>
+        <div id="list">
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+</body>
+</html>
